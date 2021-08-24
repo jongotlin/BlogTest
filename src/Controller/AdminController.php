5ac8920172004210d28ff31dfb51a5afdd85affc
Jon <jon@jon.se>
2021-08-24 20:31:26 +0200

    List users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/admin", name="admin")
      */
     public function index(): Response
     {
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $users,
         ]);
     }
 }
